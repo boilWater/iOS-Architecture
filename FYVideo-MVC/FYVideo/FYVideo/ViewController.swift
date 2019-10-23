50db22ff5a4ec343181f4fb84bb863c8a2f8661c
JackJin <1549793329@qq.com>
2019-10-23 16:28:06 +0700

    创建 MVC/MVP/MVVM 和 Cocoapods 管理项目文件路径。

---
+++ FYVideo-MVC/FYVideo/FYVideo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FYVideo
+//
+//  Created by JackJin on 2019/10/11.
+//  Copyright © 2019 xuehu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+

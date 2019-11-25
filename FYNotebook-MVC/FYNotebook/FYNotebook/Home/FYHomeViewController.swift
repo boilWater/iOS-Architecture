418672c436db79a880c077188da34f253dc6f096
Jack Bai <1549793329@qq.com>
2019-11-25 20:20:15 +0700

    MVC 项目创建类。

---
+++ FYNotebook-MVC/FYNotebook/FYNotebook/Home/FYHomeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FYHomeViewController.swift
+//  FYNotebook
+//
+//  Created by admin on 2019/11/25.
+//  Copyright © 2019 xuehu. All rights reserved.
+//
+
+import UIKit
+
+class FYHomeViewController: FYBaseViewController {
+
+    
+    // MARK: -Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: -Lazy Method
+
+}
+
+
+// MARK: - Initialized Data
+
+extension FYHomeViewController {
+    private func initializedData() {
+        
+    }
+    
+}
+
+
+// MARK: - Configure Hierarchy View
+
+extension FYHomeViewController {
+    private func configureHierarchyView() {
+        
+    }
+    
+}
+
+
+// MARK: - Private
+
+extension FYHomeViewController {
+    
+}
+
+
+

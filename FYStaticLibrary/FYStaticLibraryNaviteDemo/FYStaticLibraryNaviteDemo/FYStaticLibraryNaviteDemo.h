63496839fbac18783bcf408b576884791539417b
JackJin <1549793329@qq.com>
2020-08-19 01:52:15 +0800

    创建静态库 Floder
    创建采用 Xcode 原生创建静态库 FYStaticLibraryNativeDemo

---
+++ FYStaticLibrary/FYStaticLibraryNaviteDemo/FYStaticLibraryNaviteDemo/FYStaticLibraryNaviteDemo.h
@@ -0,0 +1,13 @@
+//
+//  FYStaticLibraryNaviteDemo.h
+//  FYStaticLibraryNaviteDemo
+//
+//  Created by JackJin on 2020/8/19.
+//  Copyright © 2020 xuehu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FYStaticLibraryNaviteDemo : NSObject
+
+@end
---
+++ FYStaticLibrary/FYStaticLibraryNaviteDemo/FYStaticLibraryNaviteDemo/FYStaticLibraryNaviteDemo.m
@@ -0,0 +1,13 @@
+//
+//  FYStaticLibraryNaviteDemo.m
+//  FYStaticLibraryNaviteDemo
+//
+//  Created by JackJin on 2020/8/19.
+//  Copyright © 2020 xuehu. All rights reserved.
+//
+
+#import "FYStaticLibraryNaviteDemo.h"
+
+@implementation FYStaticLibraryNaviteDemo
+
+@end
